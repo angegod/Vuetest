e458e869f6c3b8bd416fb7f6a86a660fe004ca27
裕安 蔡 <a0909219938@gmail.com>
2022-02-22 13:24:55 +0800

    first commit

---
+++ Vuetest/Controllers/MysqlController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data.SqlTypes;
+using Newtonsoft.Json;
+using Vuetest.Models;
+
+
+namespace Vuetest.Controllers
+{
+    public class MysqlController : Controller
+    {
+        // GET: Mysql
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test1()
+        {
+            return View();
+        }
+
+
+        public ActionResult Test2()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Test1_output(string list)
+        {
+
+            if (string.IsNullOrEmpty(list))
+            {
+                TempData["Message"] = "輸入值不可以為空值";
+            }
+
+            string connString = "server=localhost;port=3306;user=root;password=ange0909;database=vuetest;";
+
+            MySqlConnection conn = new MySqlConnection(connString);
+
+            data[] list1 = JsonConvert.DeserializeObject<data[]>(list);
+
+
+            foreach (var item in list1)
+            {
+                string insert = "insert into vuetest (nickname,name) values (@nickname,@name)";
+
+
+                MySqlCommand cmd = new MySqlCommand(insert, conn);
+
+                cmd.Parameters.Add("@nickname", MySqlDbType.VarChar, 200).Value = item.nickname;
+                cmd.Parameters.Add("@name", MySqlDbType.VarChar, 200).Value = item.name;
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+
+          
+            return RedirectToAction("Test1");
+        }
+    }
+}
---
+++ Vuetest/Models/data.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Vuetest.Models
+{
+    public class data
+    {
+        public data(int get_id,string get_nickname,string get_name)
+        {
+            id = get_id;
+            nickname = get_nickname;
+            name = get_name;
+        }
+
+
+        public int id;
+
+        public string nickname;
+
+        public string name;
+    }
+}
