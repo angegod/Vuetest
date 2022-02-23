7319c71e9ce86a78d64f95b1ab4cd974c4b5f80a
裕安 蔡 <a0909219938@gmail.com>
2022-02-24 00:26:26 +0800

    update img

---
+++ Vuetest/Controllers/ImgUploadController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data.SqlTypes;
+
+namespace Vuetest.Controllers
+{
+    public class ImgUploadController : Controller
+    {
+        // GET: ImgUpload
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Test1()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase File)
+        {
+            if (File != null && File.ContentLength > 0)
+            {
+                //存到資料夾
+                //var FileName = Path.GetFileName(File.FileName);
+                //var FilePath = Path.Combine(Server.MapPath("~/Images/"), FileName);
+                //File.SaveAs(FilePath);
+
+
+                //轉成byte 方法一 直接轉
+                byte[] FileBytes;
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    File.InputStream.CopyTo(ms);
+                    FileBytes = ms.GetBuffer();
+                }
+
+                string conn = "server=localhost;port=3306;user=root;password=ange0909;database=vuetest;";
+                string insert = "insert into imgtext (img) values(@img)";
+                MySqlConnection mycon = new MySqlConnection(conn);
+                MySqlCommand cmd = new MySqlCommand(insert, mycon);
+
+                cmd.Parameters.AddWithValue("@img", FileBytes);
+
+                mycon.Open();
+                cmd.ExecuteNonQuery();
+                mycon.Close();
+
+                TempData["Data"] = FileBytes;
+
+            }
+
+
+
+            return View();
+        }
+    }
+}
